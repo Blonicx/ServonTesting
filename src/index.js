da02e2fd542997a651e9ece22e8f8c55050b0cc3
Blonicx <belte_YT@outlook.com>
2024-10-09 13:04:25 +0200

    Updated Index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (res, req) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
